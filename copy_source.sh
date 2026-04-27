d503028154f9988e787059c120aa8fcd15801e0b
Tim Martin <timboe@gmail.com>
2026-04-27 18:02:08 +0100

    add pause and quit to minimal

--- copy_source.sh
+++ copy_source.sh
@@ -2,4 +2,5 @@ cp -r --update=none YouCannotGoBack/playdate/Source/* Source/
 cp -r --update=none Cascada/Source/* Source/
 cp -r --update=none FactoryFarming/Source/* Source/
 rm -r Source/titles
+rm Source/world_2_1.json
 cp -r PlaydateGameSelect/Source/* Source/
